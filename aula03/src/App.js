294e1be1315834f48d0f9b2c0f148d7b3b47888e
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-03-03 15:57:58 -0300

    Aula 4

---
+++ aula03/src/App.js
@@ -0,0 +1,22 @@
+import Box from './Box'
+
+function App() {
+  
+  const frutas = ["Cachorro", "Gato", "Macaco"]
+  
+  return (
+    <>
+    {
+      frutas.map((item, index)=>{
+        return(
+          <Box item={item} key={index.toString()}/>
+        )
+      })
+    }
+
+ 
+    </>
+  );
+}
+
+export default App;
---
+++ aula03/src/Box.js
@@ -0,0 +1,7 @@
+function Box(props){
+    return(
+        <li>{props.item}</li>
+    )
+}
+
+export default Box
